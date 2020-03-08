a7a2c6ce1a75e1ee7cb92d36eaa3816519ac2f1e
cristian <cristianlobomartin@gmail.com>
2020-03-08 02:23:17 +0100

    Improve script logging

--- provision/scripts/install-master.sh
+++ provision/scripts/install-master.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 set -e
 
+
 MASTER0_IP=$1
 ZK_MASTER_NUMBER=$2
+
+echo "************** INSTALLING MASTER ON $MASTER0_IP ****************"
 
 MESOS_VERSION=1.9.0
 
@@ -12,10 +15,10 @@ cd $HOME
 #sudo add-apt-repository universe
 
 echo "Installing Java"
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install openjdk-8-jdk -y
+sudo add-apt-repository ppa:openjdk-r/ppa > /dev/null 2>&1
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get upgrade -y > /dev/null 2>&1
+sudo apt-get install openjdk-8-jdk -y > /dev/null 2>&1
 
 echo "Installing Mesosphere"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
@@ -24,14 +27,14 @@ CODENAME=$(lsb_release -cs)
 echo "DISTRO=$DISTRO"
 echo "CODENAME=$CODENAME"
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
-sudo apt-get update -y
-sudo apt-get install mesosphere -y
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get install mesosphere -y > /dev/null 2>&1
 
 # Configure zookeeper
 echo "Configuring ZooKeeper"
 echo "zk://$MASTER0_IP:2181/mesos" | sudo tee /etc/mesos/zk
 echo $ZK_MASTER_NUMBER | sudo tee sudo /etc/zookeeper/conf/myid
-sudo cp /etc/zookeeper/conf/zoo.cfg /etc/zookeeper/conf/zoo.cfg.bak
+#sudo cp /etc/zookeeper/conf/zoo.cfg /etc/zookeeper/conf/zoo.cfg.bak
 # dataDir=/var/lib/zookeeper
 sudo chown zookeeper:zookeeper /var/lib/zookeeper
 echo "server.1=$MASTER0_IP:2888:3888" | sudo tee -a /etc/zookeeper/conf/zoo.cfg
@@ -46,29 +49,30 @@ sudo cp /etc/mesos-master/ip /etc/mesos-master/hostname
 
 # Configure Marathon
 echo "Configuring Marathon"
-sudo mkdir -p /etc/marathon/conf
-sudo cp /etc/mesos-master/hostname /etc/marathon/conf
-sudo cp /etc/mesos/zk /etc/marathon/conf/master
-#sudo cp /etc/marathon/conf/master /etc/marathon/conf/zk # TODO: Copy this file and replace /mesos with /marathon, then the 'cat' below is not needed
-echo "zk://$MASTER0_IP:2181/marathon" | sudo tee /etc/marathon/conf/zk
+cat << EOF > tmp
+MARATHON_MASTER=zk://$MASTER0_IP:2181/mesos
+MARATHON_ZK=zk://$MASTER0_IP:2181/marathon
+EOF
+sudo mv tmp /etc/default/marathon
+
 
 # Make masters not to run slave service
 sudo stop mesos-slave || echo "Slave service not running, good!"
 echo manual | sudo tee /etc/init/mesos-slave.override
 
 # All ready, start services
+sudo apt-get install systemd -y
 sudo restart zookeeper
-sudo start mesos-master
+sudo start mesos-master || sudo restart mesos-master
+sudo service marathon start
 
 
 #MARATHON_VERSION=1.8.222
-echo "Start Marathon"
-cd $HOME
-mkdir marathon
-curl -O https://downloads.mesosphere.io/marathon/builds/1.8.222-86475ddac/marathon-1.8.222-86475ddac.tgz
-tar xzf marathon-1.8.222-86475ddac.tgz --strip-components 1 -C marathon
-cd marathon/bin
-sudo mkdir -p /var/log/marathon
-./marathon --master $(cat /etc/mesos/zk) --zk $(cat /etc/marathon/conf/zk) > /var/log/marathon/marathon.log 2>&1 &
-
-#sudo start marathon
+#echo "Start Marathon"
+#cd $HOME
+#mkdir marathon
+#curl -O https://downloads.mesosphere.io/marathon/builds/1.8.222-86475ddac/marathon-1.8.222-86475ddac.tgz
+#tar xzf marathon-1.8.222-86475ddac.tgz --strip-components 1 -C marathon
+#cd marathon/bin
+#sudo mkdir -p /var/log/marathon
+#(./marathon --master $(cat /etc/mesos/zk) --zk $(cat /etc/marathon/conf/zk) &)
--- provision/scripts/install-slave.sh
+++ provision/scripts/install-slave.sh
@@ -3,46 +3,51 @@ set -e
 
 MASTER0_IP=$1
 SLAVE_IP=$2
+
+echo "************** INSTALLING SLAVE ON $SLAVE_IP ****************"
+
 MESOS_VERSION=1.9.0
 
 cd $HOME
-mkdir mesos
-cd mesos
 
 # Install packages
-sudo add-apt-repository universe
+#sudo add-apt-repository universe
 
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install openjdk-8-jdk -y
+echo "Installing Java"
+sudo add-apt-repository ppa:openjdk-r/ppa > /dev/null 2>&1
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get upgrade -y > /dev/null 2>&1
+sudo apt-get install openjdk-8-jdk -y > /dev/null 2>&1
 
+echo "Installing Mesosphere"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
 DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
 CODENAME=$(lsb_release -cs)
 echo "DISTRO=$DISTRO"
-echo "CODENAME=CODENAME"
+echo "CODENAME=$CODENAME"
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
-sudo apt-get update -y
-sudo apt-get install mesosphere -y
+sudo apt-get update -y > /dev/null 2>&1
+sudo apt-get install mesosphere -y > /dev/null 2>&1
 
 # Configure zookeeper
-
-cat <<EOF > sudo /etc/mesos/zk
-zk://$MASTER0_IP:2181/mesos
-EOF
-
-cat <<EOF > sudo /etc/zookeeper/conf/myid
-$ZK_MASTER_NUMBER
-EOF
-
-cat <<EOF > sudo /etc/zookeeper/conf/zoo.cfg
-server.1=$MASTER0_IP:2888:3888
-EOF
+echo "Configuring ZooKeeper"
+echo "zk://$MASTER0_IP:2181/mesos" | sudo tee /etc/mesos/zk
+echo $ZK_MASTER_NUMBER | sudo tee sudo /etc/zookeeper/conf/myid
+#sudo cp /etc/zookeeper/conf/zoo.cfg /etc/zookeeper/conf/zoo.cfg.bak
+# dataDir=/var/lib/zookeeper
+sudo chown zookeeper:zookeeper /var/lib/zookeeper
+echo "server.1=$MASTER0_IP:2888:3888" | sudo tee -a /etc/zookeeper/conf/zoo.cfg
 
 
-sudo stop zookeeper
-sudo stop mesos-master
+# Configure mesos
+echo "Configuring Mesos"
+echo 1 | sudo tee /etc/mesos-master/quorum
+echo $MASTER0_IP | sudo tee /etc/mesos-master/ip
+sudo cp /etc/mesos-master/ip /etc/mesos-master/hostname
+
+
+sudo stop zookeeper || true
+sudo stop mesos-master || true
 echo manual | sudo tee /etc/init/zookeeper.override
 echo manual | sudo tee /etc/init/mesos-master.override
 
