51aeae30b59c63453a7ded94acb7ded4e7cfd7d8
cristian <cristianlobomartin@gmail.com>
2020-03-08 20:34:11 +0100

    Install jq

--- provision/scripts/init.sh
+++ provision/scripts/init.sh
@@ -3,4 +3,4 @@ set -e
 
 sudo apt-get update -y  > /dev/null 2>&1
 sudo apt-get upgrade -y  > /dev/null 2>&1
-sudo apt-get install -y nano  > /dev/null 2>&1
+sudo apt-get install -y nano jq > /dev/null 2>&1
