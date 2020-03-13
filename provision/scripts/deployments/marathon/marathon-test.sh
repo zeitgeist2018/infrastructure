79155b45235201b30b11b7ebed49e4508ae064d5
cristian <cristianlobomartin@gmail.com>
2020-03-13 23:04:12 +0100

    Improve test marathon deployments

--- provision/scripts/deployments/marathon/marathon-test.sh
+++ provision/scripts/deployments/marathon/marathon-test.sh
@@ -2,6 +2,6 @@
 
 MARATHON_IP=192.168.1.100:8080
 
-curl -X POST http://$MARATHON_IP/v2/apps -d @marathon-server.json -H 'Content-Type: application/json'
-curl -X POST http://$MARATHON_IP/v2/apps -d @marathon-shell.json -H 'Content-Type: application/json'
-curl -X POST http://$MARATHON_IP/v2/apps -d @marathon-pacman.json -H 'Content-Type: application/json'
+curl -X PUT http://$MARATHON_IP/v2/apps/hostname -d @marathon-server.json -H 'Content-Type: application/json'
+curl -X PUT http://$MARATHON_IP/v2/apps/shell -d @marathon-shell.json -H 'Content-Type: application/json'
+curl -X PUT http://$MARATHON_IP/v2/apps/pacman -d @marathon-pacman.json -H 'Content-Type: application/json'
