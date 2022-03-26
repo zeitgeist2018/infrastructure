ef79663abd024f9483ea28bea0b9ee70db4f22c4
cristianlm <cristian.lobomartin@zooplus.com>
2022-03-26 14:49:04 +0100

    Add base ansible playbook

--- terraform/main/main.tf
+++ terraform/main/main.tf
@@ -12,15 +12,15 @@ module network {
   az      = var.account.az
 }
 
-module vms {
-  source = "./vms"
-
-  account   = var.account
-  tags      = local.common_tags
-  vpc_id    = module.network.vpc_id
-  subnet_id = module.network.subnet_ids.private_0
-  slack_webhook_url = var.slack_webhook_url
-}
+#module vms {
+#  source = "./vms"
+#
+#  account   = var.account
+#  tags      = local.common_tags
+#  vpc_id    = module.network.vpc_id
+#  subnet_id = module.network.subnet_ids.private_0
+#  slack_webhook_url = var.slack_webhook_url
+#}
 
 module budgets {
   source = "./budgets"
