69df3e0d3dbc9a4778fdf1bc7fec59da59a40989
cristianlm <cristian.lobomartin@zooplus.com>
2022-03-26 19:11:29 +0100

    Upgrade aws provider

--- terraform/config.tf
+++ terraform/config.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 1.0.11"
+  required_version = ">= 1.1.7"
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "4.2.0"
+      version = "4.8.0"
     }
   }
 
