d2d1827505da838df061161c12da2bc93a4f71f8
cristianlm <cristian.lobomartin@zooplus.com>
2022-03-27 21:38:00 +0200

    Fix common bucket creation

--- terraform/main/s3/main.tf
+++ terraform/main/s3/main.tf
@@ -1,35 +1,42 @@
+locals {
+  bucket_name = "${var.account.env}---common"
+}
 resource aws_s3_bucket common {
-  bucket = "${var.account.env}---common"
+  bucket = local.bucket_name
   tags   = merge({
     Name = "${var.account.env}-vpc"
   }, var.tags)
 }
 
+resource aws_s3_bucket_policy policy {
+  bucket = aws_s3_bucket.common.bucket
+  policy = data.aws_iam_policy_document.policy.json
+}
+
+data aws_iam_policy_document policy {
+  statement {
+    sid    = "ec2"
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    actions = [
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.bucket_name}",
+      "arn:aws:s3:::${local.bucket_name}/*"
+    ]
+  }
+}
+
 resource aws_s3_bucket_acl acl {
   bucket = aws_s3_bucket.common.bucket
   acl    = "private"
-}
-
-resource aws_s3_bucket_policy assets {
-  bucket = aws_s3_bucket.common.id
-  policy = jsonencode({
-    Version   = "2012-10-17"
-    Id        = "ec2",
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = ["*"]
-        Action : [
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:ListObjects",
-          "s3:PutObject"
-        ],
-        Resource : [
-          aws_s3_bucket.common.arn,
-          "${aws_s3_bucket.common.arn}/*"
-        ]
-      }
-    ]
-  })
 }
