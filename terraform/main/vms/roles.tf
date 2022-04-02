8c492f26d81ac3c21546e5c82bfdbac1c5b8c5d6
cristianlm <cristian.lobomartin@zooplus.com>
2022-04-02 12:31:07 +0200

    Add dynamodb permissions to nodes

--- terraform/main/vms/roles.tf
+++ terraform/main/vms/roles.tf
@@ -32,10 +32,9 @@ data aws_iam_policy_document instance_permissions {
     effect = "Allow"
 
     actions = [
-      #      "ec2:AssignPrivateIpAddresses",
       "ec2:DescribeInstances",
       "ec2:DescribeNetworkInterfaces",
-      "ec2:DescribeTags",
+      "ec2:DescribeTags"
     ]
 
     resources = [
@@ -62,7 +61,7 @@ data aws_iam_policy_document instance_permissions {
     effect = "Allow"
 
     actions = [
-      "s3:*"
+      "dynamodb:*"
     ]
 
     resources = [
