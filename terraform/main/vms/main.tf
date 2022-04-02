ed2da15f4c6b3165bd89ea5f9b8062e888431f19
cristianlm <cristian.lobomartin@zooplus.com>
2022-04-02 11:20:25 +0200

    Fix quotes

--- terraform/main/vms/main.tf
+++ terraform/main/vms/main.tf
@@ -42,8 +42,8 @@ module nodes {
     ENV               = var.account.env
     REGION            = var.account.region
     SLACK_WEBHOOK_URL = var.slack_webhook_url
-    SLACK_TOKEN = var.slack_token
+    SLACK_TOKEN       = var.slack_token
     BUCKET            = var.bucket
-    NODE_TYPE         = 'MANAGER'
+    NODE_TYPE         = "MANAGER"
   }
 }
