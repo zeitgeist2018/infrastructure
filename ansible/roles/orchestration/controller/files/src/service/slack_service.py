23b3b09d1d4c1889bf52c6b5068cf8057914a366
cristianlm <cristian.lobomartin@zooplus.com>
2022-04-02 13:07:44 +0200

    Remove try catch

--- ansible/roles/orchestration/controller/files/src/service/slack_service.py
+++ ansible/roles/orchestration/controller/files/src/service/slack_service.py
@@ -2,7 +2,6 @@ import os
 
 from slack import WebClient
 from slack.errors import SlackApiError
-from .logging_service import LoggingService as Log
 
 
 class SlackService:
@@ -12,13 +11,8 @@ class SlackService:
         self.client = WebClient(token=self.slack_token)
 
     def send_message(self, content):
-        if self.slack_token is not None:
-            try:
-                self.client.chat_postMessage(
-                    channel=self.channel,
-                    text=content
-                )
-                return True
-            except SlackApiError as e:
-                Log.err(f'Slack error: {e}')
-                return False
+        self.client.chat_postMessage(
+            channel=self.channel,
+            message=content
+        )
+        return True
